9eda829564376defa8e88ec240d88914a204e4e0
leeorta23-meet <109899549+leeorta23-meet@users.noreply.github.com>
2022-08-02 17:44:06 +0300

    s

--- database-lab/app.py
+++ database-lab/app.py
@@ -23,64 +23,63 @@ app.config['SECRET_KEY'] = 'super-secret-key'
 
 @app.route('/', methods=['GET', 'POST'])
 def signin():
-    error = ""
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
-        try:
-            login_session['user'] = auth.sign_in_with_email_and_password(email, password)
-            return redirect(url_for('add_tweet'))
-        except:
-            error = "Authentication failed"
-            return render_template("signin.html")
-    else:        
-        return render_template("signin.html")
+	error = ""
+	if request.method == 'POST':
+		email = request.form['email']
+		password = request.form['password']
+		try:
+			login_session['user'] = auth.sign_in_with_email_and_password(email, password)
+			return redirect(url_for('add_tweet'))
+		except:
+			error = "Authentication failed"
+			return render_template("signin.html")
+	else:        
+		return render_template("signin.html")
 
 
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    error = ""
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
-        full_name = request.form['full_name']
-        username = request.form['username']
-        bio = request.form['bio']
-        uid = login_session['user']['localId']
-        try:
-            login_session['user'] = auth.create_user_with_email_and_password(email, password)
+	error = ""
+	if request.method == 'POST':
+		email = request.form['email']
+		password = request.form['password']
+		full_name = request.form['full_name']
+		username = request.form['username']
+		bio = request.form['bio']
+		uid = login_session['user']['localId']
+		try:
+			login_session['user'] = auth.create_user_with_email_and_password(email, password)
 
-            user = {"email": request.form['email'], "password":request.form['password'], 
-            "full_name": request.form['full_name'], "username":request.form['username'], "bio": request.form['bio']}
-            
-            db.child("Users").child(login_session['user']['localId']).set(user)
+			user = {"email": request.form['email'], "password":request.form['password'], 
+			"full_name": request.form['full_name'], "username":request.form['username'], "bio": request.form['bio']}
+			
+			db.child("Users").child(login_session['user']['localId']).set(user)
 
-            return redirect(url_for('add_tweet'))
-        except:
-           error = "Authentication failed"
-    return render_template("signup.html")
+			return redirect(url_for('add_tweet'))
+		except:
+		   error = "Authentication failed"
+	return render_template("signup.html")
 
 
 @app.route('/add_tweet', methods=['GET', 'POST'])
 def add_tweet():
-    error = ""
-    if request.method == 'POST':
-        title = request.form["title"]
-        text = request.form["text"]
-        try:
+	error = ""
+	if request.method == 'POST':
+		title = request.form["title"]
+		text = request.form["text"]
+		try:
    
-            tweet = {"title": request.form['title'], "text":request.form['text'], "uid": login_session['user']['localId']}
-            db.child("Tweets").child(login_session['user']['localId']).push(tweet)
+			tweet = {"title": request.form['title'], "text":request.form['text'], "uid": login_session['user']['localId']}
+			db.child("Tweets").push(tweet)
 
-            return redirect(url_for('add_tweet'))
-        except:
-           error = "Authentication failed"
-    return render_template("add_tweet.html")
+			return redirect(url_for('tweets'))
+		except:
+		   print("Authentication failed")
+	return render_template("add_tweet.html")
 
 @app.route('/all_tweets', methods=['GET', 'POST'])
-def all_tweets():
-
-
+def tweets ():
+	return render_template("tweets.html", tweets = db.child("Tweets").get().val())
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
